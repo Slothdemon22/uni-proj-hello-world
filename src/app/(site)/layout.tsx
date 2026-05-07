e21e42d8201f7097b47a509b06154f877812e493
slothdemon22 <www.basilslothdemon@gmail.com>
2026-05-07 12:37:33 +0500

    HEllo from the other side

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Footer from '@/components/layout/footer';
 import Header from '@/components/layout/header/header';
+import { usePathname } from 'next/navigation';
 
 export default function SiteLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
-    <div className="dark:bg-[#101828] flex flex-col flex-1">
+    <div key={pathname} className="dark:bg-[#101828] flex flex-col flex-1">
       <Header />
       <div className="isolate flex-1 flex flex-col">{children}</div>
       <Footer />
