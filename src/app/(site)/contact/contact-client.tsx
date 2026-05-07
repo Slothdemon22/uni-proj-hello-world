f8e83163e940e33b8875f3c2e477132e67d445cc
slothdemon22 <www.basilslothdemon@gmail.com>
2026-05-07 12:15:38 +0500

    secret

--- src/app/(site)/contact/contact-client.tsx
+++ src/app/(site)/contact/contact-client.tsx
@@ -4,7 +4,7 @@ import { InputGroup } from '@/components/ui/inputs';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/inputs/textarea';
 import { z } from 'zod';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
@@ -19,6 +19,9 @@ const contactSchema = z.object({
 type ContactInputs = z.infer<typeof contactSchema>;
 
 export default function ContactClient() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   const form = useForm<ContactInputs>({
     resolver: zodResolver(contactSchema),
     defaultValues: { firstName: '', lastName: '', email: '', message: '' },
@@ -180,66 +183,68 @@ export default function ContactClient() {
         </div>
       </div>
 
-      <span className="absolute -bottom-32 left-1/2 -translate-x-1/2 z-0">
-        <svg
-          width="930"
-          height="760"
-          viewBox="0 0 930 760"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g opacity="0.3" filter="url(#filter0_f_9248_10254)">
-            <circle cx="380.335" cy="380.335" r="179.665" fill="#FF58D5" />
-          </g>
-          <g opacity="0.7" filter="url(#filter1_f_9248_10254)">
-            <circle cx="549.665" cy="380.335" r="179.665" fill="#4E6EFF" />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_9248_10254"
-              x="0.669922"
-              y="0.6698"
-              width="759.33"
-              height="759.33"
-              filterUnits="userSpaceOnUse"
-              colorInterpolationFilters="sRGB"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="100"
-                result="effect1_foregroundBlur_9248_10254"
-              />
-            </filter>
-            <filter
-              id="filter1_f_9248_10254"
-              x="170"
-              y="0.6698"
-              width="759.33"
-              height="759.33"
-              filterUnits="userSpaceOnUse"
-              colorInterpolationFilters="sRGB"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="100"
-                result="effect1_foregroundBlur_9248_10254"
-              />
-            </filter>
-          </defs>
-        </svg>
-      </span>
+      {mounted && (
+        <span className="absolute -bottom-32 left-1/2 -translate-x-1/2 z-0">
+          <svg
+            width="930"
+            height="760"
+            viewBox="0 0 930 760"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.3" filter="url(#filter0_f_9248_10254)">
+              <circle cx="380.335" cy="380.335" r="179.665" fill="#FF58D5" />
+            </g>
+            <g opacity="0.7" filter="url(#filter1_f_9248_10254)">
+              <circle cx="549.665" cy="380.335" r="179.665" fill="#4E6EFF" />
+            </g>
+            <defs>
+              <filter
+                id="filter0_f_9248_10254"
+                x="0.669922"
+                y="0.6698"
+                width="759.33"
+                height="759.33"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+              >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  stdDeviation="100"
+                  result="effect1_foregroundBlur_9248_10254"
+                />
+              </filter>
+              <filter
+                id="filter1_f_9248_10254"
+                x="170"
+                y="0.6698"
+                width="759.33"
+                height="759.33"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+              >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  stdDeviation="100"
+                  result="effect1_foregroundBlur_9248_10254"
+                />
+              </filter>
+            </defs>
+          </svg>
+        </span>
+      )}
     </section>
   );
 }
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,45 +2,52 @@ import { getCurrentYear } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
+import { useEffect, useState } from "react";
+
 export default function Footer() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   return (
     <footer className="relative overflow-hidden bg-gray-900">
-      <span className="absolute top-0 -translate-x-1/2 left-1/2">
-        <svg
-          width="1260"
-          height="457"
-          viewBox="0 0 1260 457"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter="url(#filter0_f_11105_867)">
-            <circle cx="630" cy="-173.299" r="230" fill="#3B2EFF" />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_11105_867"
-              x="0"
-              y="-803.299"
-              width="1260"
-              height="1260"
-              filterUnits="userSpaceOnUse"
-              colorInterpolationFilters="sRGB"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="200"
-                result="effect1_foregroundBlur_11105_867"
-              />
-            </filter>
-          </defs>
-        </svg>
-      </span>
+      {mounted && (
+        <span className="absolute top-0 -translate-x-1/2 left-1/2">
+          <svg
+            width="1260"
+            height="457"
+            viewBox="0 0 1260 457"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_f_11105_867)">
+              <circle cx="630" cy="-173.299" r="230" fill="#3B2EFF" />
+            </g>
+            <defs>
+              <filter
+                id="filter0_f_11105_867"
+                x="0"
+                y="-803.299"
+                width="1260"
+                height="1260"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+              >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  stdDeviation="200"
+                  result="effect1_foregroundBlur_11105_867"
+                />
+              </filter>
+            </defs>
+          </svg>
+        </span>
+      )}
       <div className="relative z-10 py-16 xl:py-24">
         <div className="container px-5 mx-auto sm:px-7">
           <div className="grid gap-y-8 gap-x-6 lg:grid-cols-12">
